fd6c150441a3b98f3468c50ccf497eec9d7bffe9
Julie Blanc <contact@julie-blanc.fr>
2025-01-07 16:57:46 +0100

    icons header

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,8 @@ window.onload = function() {
     // toggleMenu();
     toggleGrid(body);
 
+    draggable(body);
+
 
 };
 
@@ -47,5 +49,62 @@ function toggleMenu(){
         menu.classList.add('visible');
         menuToggleButton.classList.add('menu-active');
     }
+}
+
+
+
+function draggable(body){
+    console.log("draggable")
+
+
+
+
+var object = document.getElementById('element');
+var initX, initY, firstX, firstY;
+
+object.addEventListener('mousedown', function(e) {
+
+	e.preventDefault();
+	initX = this.offsetLeft;
+	initY = this.offsetTop;
+	firstX = e.pageX;
+	firstY = e.pageY;
+
+	this.addEventListener('mousemove', dragIt, false);
+
+	window.addEventListener('mouseup', function() {
+		object.removeEventListener('mousemove', dragIt, false);
+	}, false);
+
+}, false);
+
+object.addEventListener('touchstart', function(e) {
+
+	e.preventDefault();
+	initX = this.offsetLeft;
+	initY = this.offsetTop;
+	var touch = e.touches;
+	firstX = touch[0].pageX;
+	firstY = touch[0].pageY;
+
+	this.addEventListener('touchmove', swipeIt, false);
+
+	window.addEventListener('touchend', function(e) {
+		e.preventDefault();
+		object.removeEventListener('touchmove', swipeIt, false);
+	}, false);
+
+}, false);
+
+function dragIt(e) {
+	this.style.left = initX+e.pageX-firstX + 'px';
+	this.style.top = initY+e.pageY-firstY + 'px';
+}
+
+function swipeIt(e) {
+	var contact = e.touches;
+	this.style.left = initX+contact[0].pageX-firstX + 'px';
+	this.style.top = initY+contact[0].pageY-firstY + 'px';
+}
 }
 
