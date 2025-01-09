15bd31f059cd92ef41d2aa05464f2676a772fbcf
Julie Blanc <contact@julie-blanc.fr>
2025-01-09 09:28:06 +0100

    draggable

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,54 +57,63 @@ function draggable(body){
     console.log("draggable")
 
 
+    let objects = body.querySelectorAll(".draggable");
+    objects.forEach(function (object, index) {
+        console.log(object);
+
+        var initX, initY, firstX, firstY;
 
 
-var object = document.getElementById('element');
-var initX, initY, firstX, firstY;
+        object.addEventListener('mousedown', function(e) {
 
-object.addEventListener('mousedown', function(e) {
+            e.preventDefault();
+            initX = this.offsetLeft;
+            initY = this.offsetTop;
+            firstX = e.pageX;
+            firstY = e.pageY;
+        
+            this.addEventListener('mousemove', dragIt, false);
+        
+            window.addEventListener('mouseup', function() {
+                object.removeEventListener('mousemove', dragIt, false);
+            }, false);
+        
+        }, false);
 
-	e.preventDefault();
-	initX = this.offsetLeft;
-	initY = this.offsetTop;
-	firstX = e.pageX;
-	firstY = e.pageY;
+        object.addEventListener('touchstart', function(e) {
 
-	this.addEventListener('mousemove', dragIt, false);
+            e.preventDefault();
+            initX = this.offsetLeft;
+            initY = this.offsetTop;
+            var touch = e.touches;
+            firstX = touch[0].pageX;
+            firstY = touch[0].pageY;
+        
+            this.addEventListener('touchmove', swipeIt, false);
+        
+            window.addEventListener('touchend', function(e) {
+                e.preventDefault();
+                object.removeEventListener('touchmove', swipeIt, false);
+            }, false);
+        
+        }, false);
+        
+        function dragIt(e) {
+            this.style.left = initX+e.pageX-firstX + 'px';
+            this.style.top = initY+e.pageY-firstY + 'px';
+        }
+        
+        function swipeIt(e) {
+            var contact = e.touches;
+            this.style.left = initX+contact[0].pageX-firstX + 'px';
+            this.style.top = initY+contact[0].pageY-firstY + 'px';
+        }
 
-	window.addEventListener('mouseup', function() {
-		object.removeEventListener('mousemove', dragIt, false);
-	}, false);
 
-}, false);
+    });
 
-object.addEventListener('touchstart', function(e) {
 
-	e.preventDefault();
-	initX = this.offsetLeft;
-	initY = this.offsetTop;
-	var touch = e.touches;
-	firstX = touch[0].pageX;
-	firstY = touch[0].pageY;
 
-	this.addEventListener('touchmove', swipeIt, false);
 
-	window.addEventListener('touchend', function(e) {
-		e.preventDefault();
-		object.removeEventListener('touchmove', swipeIt, false);
-	}, false);
-
-}, false);
-
-function dragIt(e) {
-	this.style.left = initX+e.pageX-firstX + 'px';
-	this.style.top = initY+e.pageY-firstY + 'px';
-}
-
-function swipeIt(e) {
-	var contact = e.touches;
-	this.style.left = initX+contact[0].pageX-firstX + 'px';
-	this.style.top = initY+contact[0].pageY-firstY + 'px';
-}
 }
 
